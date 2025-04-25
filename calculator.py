72c4b763bb234ecb269bce81f34c86a64d6b0a05
Mebratu kumera <mebratu@addissoftware.com>
2025-04-25 10:43:29 +0300

    Add: calculator added

---
+++ calculator.py
@@ -0,0 +1,33 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+
+print("Select operation: +, -, *, /")
+op = input("Enter operation: ")
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if op == "+":
+    print(add(a, b))
+elif op == "-":
+    print(subtract(a, b))
+elif op == "*":
+    print(multiply(a, b))
+elif op == "/":
+    print(divide(a, b))
+else:
+    print("Invalid operation")
