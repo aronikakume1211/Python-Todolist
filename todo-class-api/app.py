e2e18bb7c54b6277fbcf45c9b0a6f95beadb2a21
Mebratu kumera <mebratu@addissoftware.com>
2025-04-25 16:34:16 +0300

    feat: swagger configuration added

--- todo-class-api/app.py
+++ todo-class-api/app.py
@@ -1,22 +1,31 @@
 from flask import Flask
+from flasgger import Swagger
 from controllers.todo_controller import todo_bp
 from controllers.auth_controller import auth_bp
 from utils.helpers import remove_pycache 
 from dotenv import load_dotenv
 
-load_dotenv() #Loads .env into environment variables
 app = Flask(__name__)
+
+#Loads .env into environment variables
+load_dotenv() 
+
+# ✅ Customized Swagger configuration
+swagger = Swagger(app, template={
+    "swagger": "2.0",
+    "info": {
+        "title": "Todo API",
+        "description": "A simple Todo API with Flask",
+        "version": "1.0.0"
+    },
+    "host": "localhost:5000",  # Change to your production host when deploying
+    "schemes": ["http"]
+})
+
+
+# Register blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(todo_bp)
-
-
-# def remove_pycache(root_folder):
-#     for root, dirs, files in os.walk(root_folder, topdown=False):
-#         for dir_name in dirs:
-#             if dir_name == '__pycache__':
-#                 dir_path = os.path.join(root, dir_name)
-#                 print(f'Removing {dir_path}')
-#                 shutil.rmtree(dir_path)
 
 if __name__ == "__main__":
     remove_pycache('./') 
--- todo-class-api/controllers/todo_controller.py
+++ todo-class-api/controllers/todo_controller.py
@@ -1,3 +1,4 @@
+from flasgger import swag_from
 from flask import Blueprint, request, jsonify
 from services.todo_service import ToDoService
 from db import db_instance
@@ -37,3 +38,72 @@ def delete_todo(id):
     if deleted:
         return jsonify({"message": "Todo deleted"})
     return jsonify({"error": "Todo not found"}), 404
+
+# http://127.0.0.1:5000/apidocs for swagger endpoint
+@todo_bp.route('/todos', methods=['GET'])
+@swag_from({
+    'tags': ['Todos'],
+    'description': 'Get all todos',
+    'responses': {
+        '200': {
+            'description': 'List of todos',
+            'schema': {
+                'type': 'array',
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'integer'},
+                        'task': {'type': 'string'},
+                        'completed': {'type': 'boolean'}
+                    }
+                }
+            }
+        }
+    }
+})
+def get_todos():
+    """
+    This endpoint returns a list of todos.
+    ---
+    """
+    todos = [
+        {"id": 1, "task": "Finish project", "completed": False},
+        {"id": 2, "task": "Read a book", "completed": True}
+    ]
+    return jsonify(todos)
+
+@todo_bp.route('/todos', methods=['POST'])
+@swag_from({
+    'tags': ['Todos'],
+    'description': 'Create a new todo',
+    'parameters': [
+        {
+            'name': 'task',
+            'in': 'body',
+            'required': True,
+            'type': 'string',
+            'description': 'The task description'
+        }
+    ],
+    'responses': {
+        '201': {
+            'description': 'Todo created successfully',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'id': {'type': 'integer'},
+                    'task': {'type': 'string'},
+                    'completed': {'type': 'boolean'}
+                }
+            }
+        }
+    }
+})
+def create_todoo():
+    """
+    This endpoint creates a new todo.
+    ---
+    """
+    new_todo = {"id": 3, "task": "Go for a walk", "completed": False}
+    return jsonify(new_todo), 201
+
